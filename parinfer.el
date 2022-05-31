9158614614a0dd6173361f90f194675b58423e76
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-05-31 23:01:58 +0900

    Release kisaragi-0.5.0; update changelog

--- parinfer.el
+++ parinfer.el
@@ -4,7 +4,7 @@
 
 ;; Author: Shi Tianshu
 ;; Homepage: https://github.com/DogLooksGood/parinfer-mode
-;; Version: 0.4.10
+;; Version: kisaragi-0.5.0
 ;; Package-Requires: ((emacs "25.1") (dash "2.13.0") (cl-lib "0.5"))
 ;; Keywords: convenience, parinfer
 
