cc80a6f8c2def2dc3f8404d478cf7168d8213c12
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-05-31 22:00:56 +0900

    Split strategies code into its own module

---
+++ parinfer-strategies.el
@@ -0,0 +1,121 @@
+;;; parinfer-strategies.el --- Strageties -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;; Entry point: parinfer-strategy-match-p
+
+;;; Code:
+
+(defvar parinfer-strategies '((default)
+                              (instantly)
+                              (skip))
+  "Parinfer invoke strategy.
+
+Use `parinfer-strategy-add' to mark functions as using a
+particular strategy. Use `parinfer-strategy-parse' to read
+values.
+
+Its elements is like below:
+
+ (STRATEGY-NAME COMMAND COMMAND ...)
+
+Each COMMAND is a symbol or a regexp string used to match commands.
+
+ strategy name    Description
+ --------------   -------------------------------------------
+ default          Invoke parinfer (delay on large sexp)
+ instantly        Invoke parinfer instantly
+ skip             Do not invoke parinfer")
+
+(defun parinfer-strategy-parse (strategy-name)
+  "Parse strategy, which is named STRATEGY-NAME in `parinfer-strategies'.
+
+Its output is a plist that looks like
+
+ (:commands cmd1 cmd2 cmd3
+  :regexps regexp1 regexp2 regexp3)"
+  (let ((list (cdr (assq strategy-name parinfer-strategies))))
+    (list :commands (cl-remove-if-not #'symbolp list)
+          :regexps  (cl-remove-if-not #'stringp list))))
+
+(defun parinfer-strategy-add (strategy &rest commands)
+  "Append new commands to STRATEGY in `parinfer-strategy'.
+
+COMMANDS is a list of commands, which may be a symbol or a regexp
+string."
+  (declare (indent 1))
+  (let* ((orig-value (cdr (assq strategy parinfer-strategies)))
+         (keys (mapcar #'car parinfer-strategies))
+         (new-value (cl-remove-duplicates
+                     (append orig-value commands)
+                     :test #'equal
+                     :from-end t))
+         output)
+    (dolist (x parinfer-strategies)
+      (if (eq (car x) strategy)
+          (push (cons strategy new-value) output)
+        (push x output)))
+    (when (not (memq strategy keys))
+      (push (cons strategy new-value) output))
+    (setq parinfer-strategies (reverse output))))
+
+(parinfer-strategy-add 'default
+  'comment-dwim
+  'comment-line
+  'comment-or-uncomment-region
+  'delete-char
+  'delete-indentation
+  'kill-line
+  'kill-region
+  'kill-word
+  'newline
+  'newline-and-indent
+  'self-insert-command
+  'evil-delete-char
+  'evil-shift-left
+  'evil-shift-left-line
+  'evil-shift-right
+  'evil-shift-right-line
+  'sp-insert-pair)
+(parinfer-strategy-add 'instantly
+  'delete-region
+  'newline
+  'evil-change
+  'evil-change-line
+  'evil-change-whole-line
+  'evil-delete
+  'evil-delete-backward-char
+  'evil-delete-char
+  'evil-delete-line
+  'evil-exit-visual-state
+  'evil-force-normal-state
+  'evil-normal-state
+  'evil-paste-after
+  'evil-paste-before
+  'evil-substitute)
+(parinfer-strategy-add 'skip
+  'evil-previous-line
+  'evil-forward-char
+  'evil-backward-char
+  'evil-next-line
+  'evil-forward-word
+  'evil-forward-word-begin
+  'evil-backward-word-begin
+  'evil-backward-end
+  'evil-scroll-page-down
+  'evil-scroll-up)
+
+(defun parinfer-strategy-match-p (command strategy-name)
+  "Return non-nil if COMMAND's parinfer invoke strategy is STRATEGY-NAME."
+  (let* ((output (parinfer-strategy-parse strategy-name))
+         (cmds (plist-get output :commands))
+         (regexps (plist-get output :regexps)))
+    (or (member command cmds)
+        (cl-some (lambda (regexp)
+                   (string-match-p
+                    regexp (symbol-name command)))
+                 regexps))))
+
+(provide 'parinfer-strategies)
+
+;;; parinfer-strategies.el ends here
--- parinfer.el
+++ parinfer.el
@@ -60,11 +60,14 @@
 ;; -----------------------------------------------------------------------------
 (require 'cl-lib)
 (require 'dash)
-(require 'parinferlib)
 (require 'mode-local)
 
 ;; for parinfer-diff
 (require 'ediff)
+
+;; Internal
+(require 'parinferlib)
+(require 'parinfer-strategies)
 
 ;; -----------------------------------------------------------------------------
 ;; Custom variables
@@ -117,26 +120,6 @@ close-parens after it.")
 
 (defvar parinfer-display-error nil
   "If display error when parinfer failed in Indent Mode.")
-
-(defvar parinfer-strategy '((default)
-                            (instantly)
-                            (skip))
-  "Parinfer invoke strategy.)
-
-This variable is an association list, user can use `parinfer-strategy-parse'
-to parse it and use `parinfer-strategy-add' to set it.
-
-Its elements is like below:
-
- (STRATEGY-NAME COMMAND COMMAND ...)
-
-Each COMMAND is a symbol or a regexp string used to match commands.
-
- strategy name    Description
- --------------   -------------------------------------------
- default          Invoke parinfer (delay on large sexp)
- instantly        Invoke parinfer instantly
- skip             Do not invoke parinfer")
 
 ;; -----------------------------------------------------------------------------
 ;; Internal variable and constants
@@ -308,102 +291,10 @@ CLAUSES are the code for lifecycle.
   "Return the current `parinfer--mode'."
   parinfer--mode)
 
-;;;; Strategies
-
-(defun parinfer-strategy-parse (strategy-name)
-  "Parse strategy, which is named STRATEGY-NAME in `parinfer-strategy'.
-
-Its output is a plist that looks like
-
- (:commands cmd1 cmd2 cmd3
-  :regexps regexp1 regexp2 regexp3)"
-  (let ((list (cdr (assq strategy-name parinfer-strategy))))
-    (list :commands (cl-remove-if-not #'symbolp list)
-          :regexps  (cl-remove-if-not #'stringp list))))
-
-(defun parinfer-strategy-add (strategy &rest commands)
-  "Append new commands to STRATEGY in `parinfer-strategy'.
-
-COMMANDS is a list of commands, which may be a symbol or a regexp
-string."
-  (declare (indent 1))
-  (let* ((orig-value (cdr (assq strategy parinfer-strategy)))
-         (keys (mapcar #'car parinfer-strategy))
-         (new-value (cl-remove-duplicates
-                     (append orig-value commands)
-                     :test #'equal
-                     :from-end t))
-         output)
-    (dolist (x parinfer-strategy)
-      (if (eq (car x) strategy)
-          (push (cons strategy new-value) output)
-        (push x output)))
-    (when (not (memq strategy keys))
-      (push (cons strategy new-value) output))
-    (setq parinfer-strategy (reverse output))))
-
-(parinfer-strategy-add 'default
-  'comment-dwim
-  'comment-line
-  'comment-or-uncomment-region
-  'delete-char
-  'delete-indentation
-  'kill-line
-  'kill-region
-  'kill-word
-  'newline
-  'newline-and-indent
-  'self-insert-command
-  'evil-delete-char
-  'evil-shift-left
-  'evil-shift-left-line
-  'evil-shift-right
-  'evil-shift-right-line
-  'sp-insert-pair)
-(parinfer-strategy-add 'instantly
-  'delete-region
-  'newline
-  'evil-change
-  'evil-change-line
-  'evil-change-whole-line
-  'evil-delete
-  'evil-delete-backward-char
-  'evil-delete-char
-  'evil-delete-line
-  'evil-exit-visual-state
-  'evil-force-normal-state
-  'evil-normal-state
-  'evil-paste-after
-  'evil-paste-before
-  'evil-substitute)
-(parinfer-strategy-add 'skip
-  'evil-previous-line
-  'evil-forward-char
-  'evil-backward-char
-  'evil-next-line
-  'evil-forward-word
-  'evil-forward-word-begin
-  'evil-backward-word-begin
-  'evil-backward-end
-  'evil-scroll-page-down
-  'evil-scroll-up)
-
-(defun parinfer--strategy-match-p (command strategy-name)
-  "Return t if COMMAND's parinfer invoke strategy is STRATEGY-NAME."
-  (let* ((output (parinfer-strategy-parse strategy-name))
-         (cmds (plist-get output :commands))
-         (regexps (plist-get output :regexps)))
-    (if (member command cmds)
-        t
-      (-any-p (lambda (regexp)
-                (string-match-p
-                 regexp (symbol-name command)))
-              regexps))))
-
 (defun parinfer--set-text-modified ()
   "Set ‘parinfer--text-modified’ to t when `this-command' use default invoke strategy."
   (when (and (symbolp this-command)
-             (parinfer--strategy-match-p this-command 'default))
+             (parinfer-strategy-match-p this-command 'default))
     (parinfer--setq-text-modified t)))
 
 (defun parinfer--switch-to-indent-mode-1 ()
@@ -582,15 +473,15 @@ POS is the position we want to call parinfer."
 
 (defun parinfer--should-skip-this-command-p ()
   "Should parinfer skip this command."
-  (parinfer--strategy-match-p this-command 'skip))
+  (parinfer-strategy-match-p this-command 'skip))
 
 (defun parinfer--should-invoke-instantly-p ()
   "Should parinfer be invoked instantly."
-  (parinfer--strategy-match-p this-command 'instantly))
+  (parinfer-strategy-match-p this-command 'instantly))
 
 (defun parinfer--should-invoke-p ()
   "Should parinfer be invoked normally."
-  (parinfer--strategy-match-p this-command 'default))
+  (parinfer-strategy-match-p this-command 'default))
 
 (defun parinfer--should-clean-up-p ()
   "Should parinfer do clean job."
@@ -660,7 +551,7 @@ This will finish delay processing immediately."
 
 (defun parinfer--update-text-modified ()
   (when (and (symbolp this-command)
-             (parinfer--strategy-match-p this-command 'default)
+             (parinfer-strategy-match-p this-command 'default)
              (not (parinfer--in-string-p)))
     (setq parinfer--text-modified t)))
 
